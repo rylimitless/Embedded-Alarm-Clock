7e5c0e52c76afbd5d622ccab22aa6bf23ac08157
ry <ry.limitless@gmail.com>
2023-10-24 23:01:38 -0500

    timers enabled

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #define F_CPU 8000000UL
 
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 #define  LCD_DATA_COMMAND_PORT    PORTB
 #define  LCD_DATA_COMMAND_DDR     DDRB
@@ -10,6 +11,12 @@
 #define RS PB1
 #define RW PB2
 #define EN PB0
+
+unsigned char seconds = 0;
+unsigned char minutes = 0;
+unsigned char hours = 0;
+
+ISR(TIMER0_COMPA_vect);
 
 uint8_t  four_bit_mode [] = {
   0x33, // 4 bit mode, 2 line, 5x7 font
@@ -34,6 +41,8 @@ void lcdStringWriter(char *);
 void lcdDataWriter(unsigned char );
 void setCursor(uint8_t , uint8_t );
 void clearDisplay();
+void intToString(int , char *);
+void initTimer();
 
 void delay_ms( uint16_t ms ){
   _delay_ms( ms);
@@ -46,11 +55,17 @@ void delay_us( uint16_t us ){
 int main(){
   _delay_ms(250);
   initLCD();
-  
+  initTimer();
+  sei();
+
+	MCUCR |= (1 << ISC11 | 1 << ISC01);
+	GIMSK |= (1 << INT0 | 1 << INT1);
   while(1){
-    // clearDisplay();
+    clearDisplay();
     setCursor(1,1);
-  lcdStringWriter("Hello World");
+    char str[10];
+    intToString(seconds, str);
+  lcdStringWriter(str);
   delay_ms(1000);
 
   }
@@ -126,4 +141,60 @@ void setCursor(uint8_t row , uint8_t col){
   unsigned char pos = lcdIndex[row-1] + col - 1;
 lcdCommandwriter(pos);
 delay_us(100);
+}
+
+ISR(TIMER1_COMPA_vect){
+  seconds++;
+  if(seconds == 60){
+    seconds = 0;
+    minutes++;
+  }
+  if(minutes == 60){
+    minutes = 0;
+    hours++;
+  }
+  if(hours == 24){
+    hours = 0;
+  }
+
+}
+
+void initTimer(){
+  TCCR1B = (1<<CS12|1<<CS10|1<<WGM12);
+	//Put appropriate value for OCR1A
+	OCR1A = 7812;
+	//Output Compare Interrupt Enabled
+	TIMSK = (1<<OCIE1A);
+}
+
+void intToString(int num, char* str) {
+    int i = 0;
+    int sign = 0;
+
+    // Handle negative numbers
+    if (num < 0) {
+        sign = 1;
+        num = -num;
+    }
+
+    // Extract digits from the number
+    do {
+        str[i++] = num % 10 + '0';
+        num /= 10;
+    } while (num > 0);
+
+    // Add sign character if necessary
+    if (sign) {
+        str[i++] = '-';
+    }
+
+    // Reverse the string
+    for (int j = 0; j < i / 2; j++) {
+        char temp = str[j];
+        str[j] = str[i - j - 1];
+        str[i - j - 1] = temp;
+    }
+
+    // Add null terminator
+    str[i] = '\0';
 }
